cd77f12af1f45577f59d2af47f3702a37fb2053d
pratan04 <prashantt.ratan04@gmail.com>
2024-04-05 01:26:01 +0530

    updates

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -42,10 +42,10 @@ const Sidebar=({selectedTab,setSelectedTab})=>{
     </ul>
     <hr/>
     <div className="dropdown">
-      <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2"/>
-        <strong>mdo</strong>
-      </a>
+      {/* <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+        <img src="" alt="" width="32" height="32" className="rounded-circle me-2"/>
+        <strong></strong>
+      </a> */}
       <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
         <li><a className="dropdown-item" href="#">New project...</a></li>
         <li><a className="dropdown-item" href="#">Settings</a></li>
